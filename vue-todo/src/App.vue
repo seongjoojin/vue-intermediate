f71c070b54aa6f40e0015e6c080d6ba2e15f5f7c
jinseongjoo <qpyou1234@naver.com>
2018-09-22 10:30:32 +0900

    파비콘, 아이콘, 폰트, 반응형 태그 설정하기

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -19,6 +19,23 @@ export default {
     TodoInput: TodoInput,
     TodoList: TodoList,
     TodoFooter: TodoFooter
+  },
+  metaInfo: {
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" }
+    ],
+    link: [
+      { rel: "icon", href: "./assets/favicon.ico", type: "image/x-icon" },
+      {
+        rel: "shortcut icon",
+        href: "./assets/favicon.ico",
+        type: "image/x-icon"
+      },
+      {
+        href: "https://fonts.googleapis.com/css?family=Ubuntu",
+        rel: "stylesheet"
+      }
+    ]
   }
 }
 </script>
