fa979df980cf6b7c9415459ce59621bc2e569e82
jinseongjoo <qpyou1234@naver.com>
2018-09-24 13:17:49 +0900

    TodoInput 컴포넌트의 할 일 저장 기능 구현

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,11 +1,24 @@
 <template>
     <div>
-        input
+        <input type="text" v-model="newTodoItem">
+        <button type="button" v-on:click="addTodo">add</button>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data: function() {
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem)
+      this.newTodoItem = ""
+    }
+  }
+}
 </script>
 
 <style>
