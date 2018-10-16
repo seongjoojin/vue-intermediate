3b89a245d90352263927611c8ff9003d5b5e38b8
jinseongjoo <qpyou1234@naver.com>
2018-10-16 20:49:03 +0900

    [리팩토링] getters와 mapGetters 적용하기

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -2,7 +2,7 @@
     <div>
        <transition-group name="list" tag="ul">
             <li
-                v-for="(todoItem, index) in this.$store.state.todoItems"
+                v-for="(todoItem, index) in this.storedTodoItems"
                 v-bind:key="todoItem.item"
                 class="shadow"
             >
@@ -23,6 +23,8 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex"
+
 export default {
   methods: {
     removeTodo(todoItem, index) {
@@ -31,6 +33,9 @@ export default {
     toggleComplete(todoItem, index) {
       this.$store.commit("toggleOneItem", { todoItem, index })
     }
+  },
+  computed: {
+    ...mapGetters(["storedTodoItems"])
   }
 }
 </script>
